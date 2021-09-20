075d474aa6356dc28ec2e463678c1700aafc340f
anderson <andersonszdc@gmail.com>
2021-09-20 07:17:36 -0300

    card de projetos estilizado

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { FaAngleRight } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
+import netflix from '../assets/netflix-mockup.jpg'
 
 const cardUp = keyframes`
     from {
@@ -14,21 +17,26 @@ const cardUp = keyframes`
 `
 
 const Wrapper = styled.div`
+    position: relative;
     display: inline-block;
     width: 33.333%;
     padding: 0 6px;
     .card__content {
+        width: 80%;
+        z-index: 2;
+        position: absolute;
+        top: 0;
+        left: 0;
         display: flex;
         flex-direction: column;
         gap: 16px;
         padding: 30px;
-        background-color: ${props => props.theme.colors.text};
-        color: ${props => props.theme.colors.background};
+        color: ${props => props.theme.colors.text};
         height: 300px;
-        border-radius: 6px;
         cursor: pointer;
         .card__title {
-            font-size: 30px;
+            white-space: normal;
+            font-size: 24px;
             font-weight: 700;
         }
         .card__verMais {
@@ -38,8 +46,26 @@ const Wrapper = styled.div`
             gap: 4px;
         }
     }
-    :hover p {
-        animation: ${cardUp} .4s ease-in-out;
+    .card__mockup {
+        width: 100%;
+        transition: .2s ease-in-out;
+        div {
+            border-radius: 6px;
+        }
+        img {
+            transition: .2s ease-in-out;
+        }
+    }
+    :hover {
+        p {
+            animation: ${cardUp} .4s ease-in-out;
+        }
+        .card__mockup {
+            opacity: 0.3;
+            img {
+                transform: scale(1.1);
+            }
+        }
     }
     @media (max-width: 800px) {
         width: 50%;
@@ -50,15 +76,20 @@ const Wrapper = styled.div`
 `
 
 const Card: React.FC = () => {
-  return (
-      <Wrapper>
-          <div className="card__content">
-            <p className="card__ano">2021</p>
-            <p className="card__title">Title</p>
-            <p className="card__verMais">Ver mais<FaAngleRight/></p>
-          </div>
-      </Wrapper>
-  );
+    return (
+        <Link passHref href='/'>
+            <Wrapper>
+                <div className="card__content">
+                    <p className="card__ano">2021</p>
+                    <p className="card__title">Netflix - Layout responsivo</p>
+                    <p className="card__verMais">Ver mais<FaAngleRight/></p>
+                </div>
+                <div className="card__mockup">
+                    <Image className='card__image' layout='responsive' src={netflix} alt='mockup' />
+                </div>
+            </Wrapper>
+        </Link>
+    );
 }
 
 export default Card;
