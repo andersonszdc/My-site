89e5dc6bd9da2411d6c663974c5587c3f4e65dbe
anderson <andersonszdc@gmail.com>
2021-09-27 12:18:06 -0300

    fix: erro de eslint no _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import Document, { DocumentContext, DocumentInitialProps } from "next/document";
 import { ReactElement } from "react";
 import { ServerStyleSheet } from "styled-components";
