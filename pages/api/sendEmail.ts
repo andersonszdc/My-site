97db8421509eac5189b531e78975d1e3bbad7391
anderson <andersonszdc@gmail.com>
2021-10-14 09:35:27 -0300

    fix: test 6

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -33,69 +33,69 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     })
 
-    // transporter.sendMail({
+    transporter.sendMail({
     
-    //       from: 'contato@andersonszdc.com',
-    //       to,
-    //       subject: "Oba! Você me enviou uma mensagem",
-    //       text: 'test'
+          from: 'contato@andersonszdc.com',
+          to,
+          subject: "Oba! Você me enviou uma mensagem",
+          // text: 'test'
+          html: `
+          <p>test 6</p>
+          `
 
-    // }).then(info => {
-    //   res.status(200).json({message: "teste 2 enviado!"})
-    //   return resolve
-    // }).catch(error => {
-    //   res.status(400).json({message: "teste não enviado!"})
-    //   return resolve
-    // })
-  
-    readFile(__dirname + '../../../../../emailAuto.html', (err, html) => {
-  
-      if (err) throw err
-  
-      // const template = handlebars.compile(`${html}`);
-      // const context = { name, to };
-      // const htmlToSend = template(context);
-  
-      transporter.sendMail({
-    
-        from: 'contato@andersonszdc.com',
-        to,
-        subject: "Oba! Você me enviou uma mensagem",
-        text: "teste 5"
-        // html: `
-        // <p>teste 4</p>
-        // `
-    
-      }).then(info => {
-        
-        // transporter.sendMail({
-          
-        //   from: 'contato@andersonszdc.com',
-        //   to: 'contato@andersonszdc.com',
-        //   replyTo: to,
-        //   subject: "Você recebeu mensagem do seu forms",
-        //   html: `
-        //   <p>${name}</p>
-        //   <p>${to}</p>
-        //   <p>${message}</p>
-        //   `
-          
-        // }).then(info => {
-        //     res.status(200).json({message: 'Mensagem enviada!', info})
-        //     return resolve
-        //   }).catch(error => {
-        //     res.status(400).json({message: 'Erro no aviso!', error})
-        //     return resolve
-        //   })
-        res.status(200).json({message: "teste 5 enviado!"})
-        return resolve
-  
-      }).catch(error => {
-        res.status(400).json({message: 'Erro na mensagem!', error})
-        return resolve
-      })
-  
+    }).then(info => {
+      res.status(200).json({message: "teste 6 enviado!"})
+      return resolve
+    }).catch(error => {
+      res.status(400).json({message: "teste não enviado!"})
+      return resolve
     })
+  
+    // readFile(__dirname + '../../../../../emailAuto.html', (err, html) => {
+  
+    //   if (err) throw err
+  
+    //   const template = handlebars.compile(`${html}`);
+    //   const context = { name, to };
+    //   const htmlToSend = template(context);
+  
+    //   transporter.sendMail({
+    
+    //     from: 'contato@andersonszdc.com',
+    //     to,
+    //     subject: "Oba! Você me enviou uma mensagem",
+    //     html: htmlToSend
+    
+    //   }).then(info => {
+        
+    //     transporter.sendMail({
+          
+    //       from: 'contato@andersonszdc.com',
+    //       to: 'contato@andersonszdc.com',
+    //       replyTo: to,
+    //       subject: "Você recebeu mensagem do seu forms",
+    //       html: `
+    //       <p>${name}</p>
+    //       <p>${to}</p>
+    //       <p>${message}</p>
+    //       `
+          
+    //     }).then(info => {
+    //         res.status(200).json({message: 'Mensagem enviada!', info})
+    //         return resolve
+    //       }).catch(error => {
+    //         res.status(400).json({message: 'Erro no aviso!', error})
+    //         return resolve
+    //       })
+    //     res.status(200).json({message: "teste 5 enviado!"})
+    //     return resolve
+  
+    //   }).catch(error => {
+    //     res.status(400).json({message: 'Erro na mensagem!', error})
+    //     return resolve
+    //   })
+  
+    // })
 
   })
 
