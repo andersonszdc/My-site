90f9ddba79413b489be6d86e93e8ab77833d2dc9
anderson <andersonszdc@gmail.com>
2021-10-14 10:39:47 -0300

    fix: test 7

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer'
-import {readFile} from 'fs'
+import fs, {readFile} from 'fs'
 import handlebars from 'handlebars'
 import isEmailValid from '../../utils/isEmailValid'
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -23,79 +23,79 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return resolve
     }
   
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
-      }
-    })
-
-    transporter.sendMail({
-    
-          from: 'contato@andersonszdc.com',
-          to,
-          subject: "Oba! Você me enviou uma mensagem",
-          // text: 'test'
-          html: `
-          <p>test 6</p>
-          `
-
-    }).then(info => {
-      res.status(200).json({message: "teste 6 enviado!"})
-      return resolve
-    }).catch(error => {
-      res.status(400).json({message: "teste não enviado!"})
-      return resolve
-    })
-  
-    // readFile(__dirname + '../../../../../emailAuto.html', (err, html) => {
-  
-    //   if (err) throw err
-  
-    //   const template = handlebars.compile(`${html}`);
-    //   const context = { name, to };
-    //   const htmlToSend = template(context);
-  
-    //   transporter.sendMail({
-    
-    //     from: 'contato@andersonszdc.com',
-    //     to,
-    //     subject: "Oba! Você me enviou uma mensagem",
-    //     html: htmlToSend
-    
-    //   }).then(info => {
-        
-    //     transporter.sendMail({
-          
-    //       from: 'contato@andersonszdc.com',
-    //       to: 'contato@andersonszdc.com',
-    //       replyTo: to,
-    //       subject: "Você recebeu mensagem do seu forms",
-    //       html: `
-    //       <p>${name}</p>
-    //       <p>${to}</p>
-    //       <p>${message}</p>
-    //       `
-          
-    //     }).then(info => {
-    //         res.status(200).json({message: 'Mensagem enviada!', info})
-    //         return resolve
-    //       }).catch(error => {
-    //         res.status(400).json({message: 'Erro no aviso!', error})
-    //         return resolve
-    //       })
-    //     res.status(200).json({message: "teste 5 enviado!"})
-    //     return resolve
-  
-    //   }).catch(error => {
-    //     res.status(400).json({message: 'Erro na mensagem!', error})
-    //     return resolve
-    //   })
-  
+    // const transporter = nodemailer.createTransport({
+    //   host: 'smtp.gmail.com',
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: process.env.GMAIL_USER,
+    //     pass: process.env.GMAIL_PASS
+    //   }
     // })
+
+    // transporter.sendMail({
+    
+    //       from: 'contato@andersonszdc.com',
+    //       to,
+    //       subject: "Oba! Você me enviou uma mensagem",
+    //       // text: 'test'
+    //       html: `
+    //       <p>test 6</p>
+    //       `
+
+    // }).then(info => {
+    //   res.status(200).json({message: "teste 6 enviado!"})
+    //   return resolve
+    // }).catch(error => {
+    //   res.status(400).json({message: "teste não enviado!"})
+    //   return resolve
+    // })
+  
+    readFile(__dirname + '../../../../../emailAuto.html', (err, html) => {
+  
+      if (err) throw err
+      
+      res.status(200).json({message: 'test 7 enviada!'})
+
+      // const template = handlebars.compile(`${html}`);
+      // const context = { name, to };
+      // const htmlToSend = template(context);
+  
+      // transporter.sendMail({
+    
+      //   from: 'contato@andersonszdc.com',
+      //   to,
+      //   subject: "Oba! Você me enviou uma mensagem",
+      //   html: htmlToSend
+    
+      // }).then(info => {
+        
+      //   transporter.sendMail({
+          
+      //     from: 'contato@andersonszdc.com',
+      //     to: 'contato@andersonszdc.com',
+      //     replyTo: to,
+      //     subject: "Você recebeu mensagem do seu forms",
+      //     html: `
+      //     <p>${name}</p>
+      //     <p>${to}</p>
+      //     <p>${message}</p>
+      //     `
+          
+      //   }).then(info => {
+      //       res.status(200).json({message: 'Mensagem enviada!', info})
+      //       return resolve
+      //     }).catch(error => {
+      //       res.status(400).json({message: 'Erro no aviso!', error})
+      //       return resolve
+      //     })
+  
+      // }).catch(error => {
+      //   res.status(400).json({message: 'Erro na mensagem!', error})
+      //   return resolve
+      // })
+  
+    })
 
   })
 
