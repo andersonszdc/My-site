a26b978db23db19e9b4b94a4d2b19239bc42aa47
anderson <andersonszdc@gmail.com>
2021-10-19 16:14:06 -0300

    fix: add domain in images at next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.prismic.io', 'images2.imgbox.com'],
+    domains: ['images.prismic.io', 'images2.imgbox.com', 'https://lh3.googleusercontent.com'],
   },
   experimental: {
     nftTracing: true
