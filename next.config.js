c93ebce40e4f303fa4656dc6bdbc4e195cccd45a
anderson <andersonszdc@gmail.com>
2021-11-01 17:41:29 -0300

    updated next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.prismic.io', 'images2.imgbox.com', 'my-site-andersonszdc.s3.sa-east-1.amazonaws'],
+    domains: ['images.prismic.io', 'images2.imgbox.com', 'my-site-andersonszdc.s3.sa-east-1.amazonaws.com'],
   },
   experimental: {
     nftTracing: true
