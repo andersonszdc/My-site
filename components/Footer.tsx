28f6a57ffe5b4708b5c172377f1c8174de0b85cf
anderson <andersonszdc@gmail.com>
2021-11-26 23:13:04 -0300

    feat: updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,49 +1,61 @@
 import React from 'react'
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 import styled from 'styled-components'
 import Logo from './Logo';
 
 const Wrapper = styled.footer`
   display: flex;
   flex-direction: column;
-  padding: 0 4% 1.6rem 4%;
+  margin: 0 4% 1.6rem 4%;
+  align-items: center;
+
   span {
     display: block;
     border-top: 0.1rem solid white;
     width: 100%;
     margin-bottom: 9rem;
   }
-  .content {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-  }
-  .title {
-    margin-bottom: 2.4rem;
-    font-size: 2.4rem;
-  }
-  .box {
-    display: flex;
-    flex-direction: column;
-    gap: 1.6rem;
-  }
-  .item {
-    display: flex;
-    align-items: center;
-    gap: .6rem;
-    font-weight: 500;
-    font-size: 1.6rem;
 
-    :before {
-      content: '';
-      display: flex;
-      position: relative;
-      margin: 0;
-      width: 2rem;
-      height: .3rem;
-      background-color: ${props => props.theme.colors.blue};
-      border-radius: 1.2rem;
+  .footer__abas {
+    display: flex;
+    gap: 6.4rem;
+  }
+
+  .abas__item {
+    font-size: 1.6rem;
+    font-weight: 600;
+    cursor: pointer;
+    transition: .2s ease-out;
+
+    :hover {
+      color: ${props => props.theme.colors.blue};
     }
   }
+
+  .redes__title {
+    font-size: 1.6rem;
+    font-weight: 600;
+    text-align: center;
+    margin-top: 3.2rem;
+    margin-bottom: 1.6rem;
+  }
+
+  .footer__icons {
+    display: flex;
+    gap: 2.4rem;
+  }
+
+  .icon {
+    font-size: 3rem;
+    cursor: pointer;
+    transition: .2s ease-out;
+
+    :hover {
+      color: ${props => props.theme.colors.blue};
+    }
+  }
+
   .credit {
     margin-top: 6rem;
     display: flex;
@@ -55,30 +67,9 @@ const Wrapper = styled.footer`
       color: ${props => props.theme.colors.blue};
     }
   }
+
   .logo {
     width: 2.5rem;
-  }
-
-  @media (max-width: 900px) {
-    .content {
-      flex-direction: column;
-      gap: 4.5rem;
-    }
-  }
-
-  @media (max-width: 400px) {
-    span {
-      margin-bottom: 3.2rem;
-    }
-    .content {
-      gap: 3.2rem;
-    }
-    .title {
-      font-size: 1.8rem;
-    }
-    .item {
-      font-size: 1.2rem;
-    }
   }
 `
 
@@ -86,30 +77,21 @@ const Footer: React.FC = () => {
   return (
     <Wrapper>
       <span></span>
+      <ul className="footer__abas">
+        <li className="abas__item">Contatos</li>
+        <li className="abas__item">Projetos</li>
+        <li className="abas__item">Blog</li>
+        <li className="abas__item">Sobre mim</li>
+      </ul>
       <div>
-        <div className='content'>
-          <div>
-            <h3 className='title'>Contatos</h3>
-            <ul className='box'>
-              <li className='item'>andersonszdc@gmail.com</li>
-              <li className='item'>+55 (21) 97351-2104</li>
-            </ul>
-          </div>
-          <div>
-            <h3 className='title'>Posts recentes</h3>
-            <ul className='box'>
-              <li className='item'>Next.js - Meu resumo...</li>
-              <li className='item'>TypeScript - Meu resumo...</li>
-            </ul>
-          </div>
-          <div>
-            <h3 className='title'>Minhas redes</h3>
-            <ul className='box'>
-              <li className='item'>Instagram</li>
-              <li className='item'>GitHub</li>
-            </ul>
-          </div>
-        </div>
+        <h3 className="redes__title">Minhas redes</h3>
+        <ul className="footer__icons">
+          <li><FaInstagram className="icon" /></li>
+          <li><FaTwitter className="icon" /></li>
+          <li><HiOutlineMail className="icon" /></li>
+          <li><FaLinkedin className="icon" /></li>
+          <li><FaGithub className="icon" /></li>
+        </ul>
       </div>
       <div className='credit'>
         <div className='logo'>
