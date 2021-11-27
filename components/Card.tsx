cfa6ec9bec76b1222b99672af1b1883e94a554f5
anderson <andersonszdc@gmail.com>
2021-11-26 22:42:55 -0300

    feat: add cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,147 +1,63 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import { FaAngleRight } from 'react-icons/fa'
 import Image from 'next/image'
 import Link from 'next/link'
-import deliveryCover from '../assets/delivery-cover.png';
-import siteCover from '../assets/site-cover.png';
-
-const cardUp = keyframes`
-    from {
-        transform: translateY(30px);
-        opacity: 0;
-    }
-    to {
-        transform: translateY(0);
-        opacity: 1;
-    }
-`
+import TechIcons, { TechListType } from './TechIcons'
+import UnstyledLink from './links/UnstyledLink'
 
 const Wrapper = styled.div`
-    position: relative;
-    display: inline-block;
-    width: 33.333%;
-    padding: 0 .6rem;
-    cursor: pointer;
-
-    .card__mockup {
-        transition: .2s ease-in-out;
-        img {
-            border-radius: .6rem;
-            transition: .2s ease-in-out;
-        }
-    }
-
-    :hover {
-        p {
-            animation: ${cardUp} .4s ease-in-out;
-        }
-        .card__verMais {
-            display: inline-flex;
-        }
-        .card__mockup {
-            opacity: 0.3;
-            img {
-                transform: scale(1.1);
-            }
-        }
-    }
-
-    @media (max-width: 816px) {
-        width: 50%;
-    }
-
-    @media (max-width: 502px) {
-        width: 100%;
-    }
-`
-
-const CardContent = styled.div`
-    cursor: pointer;
-    width: 80%;
-    z-index: 2;
-    position: absolute;
-    top: 0;
-    left: 0;
     display: flex;
     flex-direction: column;
-    gap: 1.6rem;
-    padding: 3rem;
-    color: ${props => props.theme.colors.text};
-    height: 30rem;
+    width: 100%;
+    height: 100%;
+    padding: 1.6rem;
+    cursor: pointer;
+    border-radius: .8rem;
+    border: 1px solid white;
+    transition: .1s ease-in-out;
+    :hover {
+        transform: scale(1.015);
+    }
 
-    .card__ano {
-        font-size: 1.6rem;
-        font-weight: 500;
-        color: ${props => props.theme.colors.blue};
-    }
     .card__title {
-        white-space: normal;
-        font-size: 2.4rem;
-        font-weight: 700;
+        font-size: 2rem;
+        font-weight: 600;
     }
-    .card__verMais {
-        cursor: pointer;
-        font-weight: 500;
-        font-size: 1.6rem;
+
+    .card__description {
+        font-size: 1.4rem;
+        font-weight: 400;
+        margin-top: .4rem;
+        margin-bottom: auto;
+    }
+
+    .card__image {
+        border-radius: .4rem;
+    }
+
+    .card__ver {
+        margin-top: .8rem;
         display: flex;
         align-items: center;
-        gap: 4px;
-        display: none;
-    }
-
-    @media (max-width: 1008px) {
-        .card__title {
-            font-size: 1.8rem;
-        }
-    }
-
-    @media (max-width: 816px) {
-        .card__title {
-            font-size: 2.4rem;
-        }
-    }
-
-    @media (max-width: 672px) {
-        .card__title {
-            font-size: 2rem;
-        }
-    }
-
-    @media (max-width: 586px) {
-        .card__title {
-            font-size: 1.6rem;
-        }
-    }
-
-    @media (max-width: 502px) {
-        .card__title {
-            font-size: 2.2rem;
-        }
-    }
-
-    @media (max-width: 314px) {
-        .card__title {
-            font-size: 1.6rem;
-        }
+        gap: .4rem;
+        font-size: 1.6rem;
+        font-weight: 600;
     }
 `
 
 const Card = ({data}: any) => {
     console.log(data)
     return (
-        // <Link passHref href={data.link}>
+        <Link passHref href={data.link}>
             <Wrapper>
-                <CardContent>
-                    <p className="card__ano">{data.slug}</p>
                     <p className="card__title">{data.title}</p>
-                    <p className="card__verMais">Ver mais<FaAngleRight/></p>
-                </CardContent>
-                {/* <div className="card__mockup">
-                    <Image className='card__image' layout='responsive' src={data.cover} alt='mockup' />
-                </div> */}
+                    <p className="card__description">{data.description}</p>
+                    <TechIcons techs={data.techs.split(',') as Array<TechListType>} />
+                    <Image className='card__image' width={400} height={250} layout='responsive' src={data.url} alt='mockup' />
+                    <p className="card__ver">Ver mais<FaAngleRight/></p>
             </Wrapper>
-        // </Link>
+        </Link>
     );
 }
 
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,14 +20,9 @@ const Wrapper = styled.div`
     }
 
     .projects__cards {
-        display: block;
-        white-space: nowrap;
-        overflow: hidden;
-    }
-
-    .projects__slider {
-        display: block;
-        transition: 0.6s ease-in-out;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 2.4rem;
     }
 
     @media (max-width: 400px) {
@@ -38,83 +33,15 @@ const Wrapper = styled.div`
     }
 `
 
-const AnglesBtn = styled.div<StyledProps>`
-    display: flex;
-    justify-content: center;
-    gap: 12px;
-    font-size: 40px;
-    margin-top: 24px;
-
-    .angleLeft, .angleRight {
-        cursor: pointer;
-        transition: opacity .5s ease-in-out;
-    }
-
-    ${({leftIsActive}) => !leftIsActive && `
-        .angleLeft {
-            cursor: default;
-            opacity: 0.2;
-        }
-    `}
-
-    ${({rightIsActive}) => !rightIsActive && `
-        .angleRight {
-            cursor: default;
-            opacity: 0.2;
-        }
-    `}
-`
-
 const Projects = ({projects}: any) => {
-    const [ translate, setTranslate ] = useState(0)
-    const [ leftIsActive, setleftIsActive ] = useState(true)
-    const [ rightIsActive, setRightIsActive ] = useState(true)
-    const [ width, setWidth ] = useState(0)
-    const [ scrollwidth, setScrollsetWidth ] = useState(0)
-    const slider = useRef<HTMLDivElement>(null)
-
-    function handleRight() {
-        if(rightIsActive===true) {
-            setTranslate(translate - width)
-        }
-    }
-
-    function handleLeft() {
-        if (leftIsActive===true) {
-            setTranslate(translate + width)
-        }
-    }
-
-    useEffect(() => {
-        if(null !== slider.current) {
-            setScrollsetWidth(slider.current.scrollWidth)
-            setWidth(slider.current.clientWidth)
-            slider.current.style.transform = `translateX(${translate}px)`
-        }
-    }, [slider, translate])
-
-    useEffect(() => {
-            setleftIsActive(translate===0?false:true)
-            if((scrollwidth <= width - translate)) {
-                setRightIsActive(false)
-            } else {
-                setRightIsActive(true)
-            }
-    }, [translate, width, scrollwidth])
 
     return (
         <Wrapper>
             <h2 className="projects_title">Projetos</h2>
             <div className="projects__cards">
-                <div ref={slider} className="projects__slider">
-                    {projects.map((project: any, index: number) => (
-                        <Card data={project} key={index} />
-                    ))}
-                </div>
-                <AnglesBtn rightIsActive={rightIsActive} leftIsActive={leftIsActive} >
-                    <FaAngleLeft className="angleLeft" onClick={handleLeft} />
-                    <FaAngleRight className="angleRight" onClick={handleRight} />
-                </AnglesBtn>
+                {projects.map((project: any, index: number) => (
+                    <Card data={project} key={index} />
+                ))}
             </div>
         </Wrapper>
     );
