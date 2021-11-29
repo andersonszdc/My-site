bd601d94820efb5cee8fc7069f53e04595d45cd2
anderson <andersonszdc@gmail.com>
2021-11-29 08:49:51 -0300

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import Layout from '../components/Layout'
 import Global from '../styles/global'
 import pattern from '../styles/themes/pattern'
 import menu from '../styles/themes/menu'
 import '../styles/mdx.css'
+import { useRouter } from 'next/dist/client/router'
+import { getAnalytics, logEvent } from "firebase/analytics";
+import app from '../firebase/config'
 
 interface Itheme {
   title: string,
-
+  
   colors: {
     text: string,
     background: string
@@ -18,11 +21,18 @@ interface Itheme {
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  
+  
+  const route = useRouter()
   const [theme, setTheme] = useState<Itheme>(pattern)
-
+  
   const toggleTheme = () => {
     setTheme(theme.title === 'pattern' ? menu : pattern)
   }
+  useEffect(() => {
+    const analytics = getAnalytics(app)
+    logEvent(analytics, `${route.asPath}`)
+  })
 
   return (
     <>
