a5753d8830d6c465cc9cdb4e4c230e8ffd8152eb
anderson <andersonszdc@gmail.com>
2021-11-29 08:48:36 -0300

    updated firebase config

--- firebase/config.tsx
+++ firebase/config.tsx
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -13,5 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
+
+export default app
