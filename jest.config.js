0252121202a99cd05a4d62af977bfeb0abe56dde
anderson <andersonszdc@gmail.com>
2021-12-07 14:28:51 -0300

    configurated jest and added a test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest'
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
     dir: './',
@@ -8,4 +8,4 @@ const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
 
-export default createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig)
