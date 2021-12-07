d40f9bae3b22b781daba6a376026321a1a6f07f4
anderson <andersonszdc@gmail.com>
2021-12-07 19:16:21 -0300

    updated types in ProjectFrontmatter

--- types/frontmatters.ts
+++ types/frontmatters.ts
@@ -26,15 +26,11 @@ export type LibraryFrontmatter = {
 export type ProjectFrontmatter = {
     slug: string;
     title: string;
-    publishedAt: string;
-    lastUpdated?: string;
     description: string;
     category?: string;
     techs: string;
-    banner: string;
     link?: string;
     github?: string;
-    youtube?: string;
     cover: string;
 };
 
