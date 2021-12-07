e27edbfad411cdab75fd85f69e2f93804468ffb0
anderson <andersonszdc@gmail.com>
2021-12-07 09:44:17 -0300

    converted to ES6 modules

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,2 @@
-module.exports = {
-    "presets": ["next/babel"],
-    "plugins": [["styled-components", { "ssr": true }]]
-};
+export const presets = ["next/babel"];
+export const plugins = [["styled-components", { "ssr": true }]];
--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['my-site-andersonszdc.s3.sa-east-1.amazonaws.com', 'images.unsplash.com', 'firebasestorage.googleapis.com'],
-  },
-  experimental: {
-    nftTracing: true
-  }
-}
+export const reactStrictMode = true;
+export const images = {
+  domains: ['my-site-andersonszdc.s3.sa-east-1.amazonaws.com', 'images.unsplash.com', 'firebasestorage.googleapis.com'],
+};
+export const experimental = {
+  nftTracing: true
+};
