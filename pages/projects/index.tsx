25b65442886884efef7de5188d8644c4ef51be85
anderson <andersonszdc@gmail.com>
2021-12-07 19:13:39 -0300

    added projects page

---
+++ pages/projects/index.tsx
@@ -0,0 +1,73 @@
+import { GetStaticProps } from 'next';
+import React from 'react';
+import { getAllFilesFrontmatter } from '../../lib/mdx';
+import {ProjectFrontmatter} from '../../types/frontmatters'
+import Card from '../../components/Card'
+import { getDownloadURL, ref } from '@firebase/storage'
+import { storage } from '../../firebase/config'
+import styled from 'styled-components';
+
+type IndexProps = {
+    projects: ProjectFrontmatter[]
+}
+
+const ProjectsGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2.4rem;
+    margin: 2.4rem 0;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 3.2rem 4%;
+
+    .projects__title {
+        font-size: 2.4rem;
+        font-weight: 700;
+    }
+
+    .projects__subtitle {
+        font-size: 1.4rem;
+        font-weight: 500;
+        margin-top: .8rem;
+    }
+`
+
+const Index = ({projects}: IndexProps) => {
+    return (
+        <Wrapper>
+            <h1 className="projects__title">Projetos</h1>
+            <h2 className="projects__subtitle">Projetos pessoais, de clientes e estudos de caso.</h2>
+            <ProjectsGrid>
+                {projects.map((project, index) => (
+                    <Card data={project} key={index} />
+                ))}
+            </ProjectsGrid>
+        </Wrapper>
+    );
+}
+
+export default Index;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const projects = await getAllFilesFrontmatter('projects')
+
+  let projectsWithUrl: any = []
+
+  const searchUrl = async (project: any) => {
+      const fileName = project.cover
+      const imageRef = ref(storage, fileName)
+      const cover = await getDownloadURL(imageRef)
+      projectsWithUrl.push({...project, cover})
+    }
+
+  for (let project of projects) {
+    await searchUrl(project)
+  }
+
+  return {
+    props: { projects: projectsWithUrl }
+  }
+}
