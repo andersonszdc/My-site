1dc54cf2ec541b3e9d751d582f38cffbd12ac8fd
anderson <andersonszdc@gmail.com>
2021-12-07 09:43:42 -0300

    added jest and testing-library

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
+
+export default createJestConfig(customJestConfig)
