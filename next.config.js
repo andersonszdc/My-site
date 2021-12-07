62c618592ec4ca33314e9854fa621564117093ed
anderson <andersonszdc@gmail.com>
2021-12-07 10:02:33 -0300

    switched to SWC

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     domains: ['my-site-andersonszdc.s3.sa-east-1.amazonaws.com', 'images.unsplash.com', 'firebasestorage.googleapis.com'],
   },
   experimental: {
-    nftTracing: true
+    nftTracing: true,
+    styledComponents: true,
   }
 }
