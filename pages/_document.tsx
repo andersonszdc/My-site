bee208e0e52e5c813c64f6b70cdfe98881dbef8f
anderson <andersonszdc@gmail.com>
2021-12-14 10:29:27 -0300

    updated metadatas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,9 +43,22 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="keyword" content="Desenvolvedor Web, Front End, Full Stack, Programador, Javascript, JS, CSS, UI Designer, UX Designer, UI/UX."/>
+          {/* Primary Meta tags */}
+          <meta name="title" content="Andersonszdc" />
           <meta name="description" content="Desenvolvedor Full Stack e UI/UX Designer, compartilho meus projetos e faço posts sobre partes interessantes da tecnologia." />
-          <meta name="image" content="https://firebasestorage.googleapis.com/v0/b/my-site-1aa62.appspot.com/o/metatag.png?alt=media&token=6a4efdd5-ba48-48c0-aeee-dacc7e263f30" />
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.andersonszdc.com/" />
+          <meta property="og:title" content="Andersonszdc" />
+          <meta property="og:description" content="Desenvolvedor Full Stack e UI/UX Designer, compartilho meus projetos e faço posts sobre partes interessantes da tecnologia." />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/my-site-1aa62.appspot.com/o/metatag.png?alt=media&token=6a4efdd5-ba48-48c0-aeee-dacc7e263f30" />
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://www.andersonszdc.com/" />
+          <meta property="twitter:title" content="Andersonszdc" />
+          <meta property="twitter:description" content="Desenvolvedor Full Stack e UI/UX Designer, compartilho meus projetos e faço posts sobre partes interessantes da tecnologia." />
+          <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/my-site-1aa62.appspot.com/o/metatag.png?alt=media&token=6a4efdd5-ba48-48c0-aeee-dacc7e263f30" />
+          {/* Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
