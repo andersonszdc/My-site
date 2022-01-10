aa621ea70a6b1e3580c385e549ae431e7bc3e7da
anderson <andersonszdc@gmail.com>
2022-01-10 06:33:04 -0300

    switched email api for sendgrid

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,82 +1,57 @@
-import nodemailer from 'nodemailer'
-import fs, {readFile} from 'fs'
-import handlebars from 'handlebars'
-import isEmailValid from '../../utils/isEmailValid'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from "nodemailer";
+import { readFile } from "fs";
+import handlebars from "handlebars";
+import isEmailValid from "../../utils/isEmailValid";
+import sgMail from "@sendgrid/mail";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-  const { name, to, message } = req.body
+  const { name, to, message } = req.body;
 
-  return new Promise(resolve => {
+  const msgToCustomer = {
+    to,
+    from: {
+      email: "contato@andersonszdc.com",
+      name: "Anderson 💻",
+    },
+    templateId: "d-965f81b126d7452f9ea760a940954230",
+    dynamicTemplateData: {
+      name,
+      to,
+    },
+  };
 
-    if (isEmailValid(to) === false) {
-      res.status(400).json({message: 'E-mail incorreto'})
-      return resolve
-    }
-  
-    if (!name || !message) {
-      res.status(400).json({message: 'Nome/mensagem faltando'})
-      return resolve
-    }
-  
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
-      }
-    })
-  
-    readFile(process.cwd() + '/emailAuto.html', (err, html) => {
-  
-      if (err) throw err
+  const msgForMe = {
+    to: "contato@andersonszdc.com",
+    from: {
+      email: "contato@andersonszdc.com",
+      name: "SITE_FORMS",
+    },
+    html: `<h1>${name}</h1><h1>${to}</h1><p>${message}</p>`,
+  };
 
-      const template = handlebars.compile(`${html}`);
-      const context = { name, to };
-      const htmlToSend = template(context);
-  
-      transporter.sendMail({
-    
-        from: 'Anderson Souza <contato@andersonszdc.com>',
-        to,
-        subject: "Oba! Você me enviou uma mensagem",
-        html: htmlToSend
-    
-      }).then(info => {
-        
-        transporter.sendMail({
-          
-          from: 'contato@andersonszdc.com',
-          to: 'contato@andersonszdc.com',
-          replyTo: to,
-          subject: "Você recebeu mensagem do seu forms",
-          html: `
-          <p>${name}</p>
-          <p>${to}</p>
-          <p>${message}</p>
-          `
-          
-        }).then(info => {
-            res.status(200).json({sucess: 'Mensagem enviada!', info})
-            return resolve
-          }).catch(error => {
-            res.status(400).json({message: 'Erro no aviso!', error})
-            return resolve
-          })
-  
-      }).catch(error => {
-        res.status(400).json({message: 'Erro na mensagem!', error})
-        return resolve
-      })
-  
-    })
-
-  })
-
+  sgMail
+    .send(msgForMe)
+    .then(() =>
+      sgMail
+        .send(msgToCustomer)
+        .then(() => res.status(200).json({ sucess: "Mensagem enviada!" }))
+        .catch((error) =>
+          res.status(400).json({ message: "Erro na mensagem de confirmação!", error })
+        )
+    )
+    .catch((error) =>
+      res.status(400).json({ message: "Erro no aviso!", error })
+    );
 }
