6061177898323ec379573026567add8f49b38e05
anderson <andersonszdc@gmail.com>
2022-01-11 15:39:42 -0300

    removed react-copy from files

--- components/content/MDXComponent.tsx
+++ components/content/MDXComponent.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import CustomCode, { Pre } from './CustomCode';
 import TechIcons from '../TechIcons';
 import CustomLink from '../links/CustomLink';
 import CustomList from './CustomList';
