b413d7327e1bb6d8411a6ee506766bceff90ca07
anderson <andersonszdc@gmail.com>
2022-01-11 08:51:49 -0300

    formatted code

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,188 +1,187 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import {getMDXComponent } from 'mdx-bundler/client';
-import React, { useEffect, useMemo } from 'react';
-import { getFileBySlug, getFiles } from '../../lib/mdx';
-import MDXComponents from '../../components/content/MDXComponent';
-import TableOfContents, { HeadingScrollSpy } from '../../components/content/TableOfContents';
-import useScrollSpy from '../../hooks/useScrollspy';
-import styled from 'styled-components';
-import CustomLink from '../../components/links/CustomLink';
-import { SiGithub } from 'react-icons/si';
-import { HiLink } from 'react-icons/hi';
-import { ProjectFrontmatter } from '../../types/frontmatters';
-import { storage } from '../../firebase/config';
-import { ref } from '@firebase/storage';
-import { getDownloadURL } from 'firebase/storage';
-import Image from 'next/image';
+import { GetStaticPaths, GetStaticProps } from "next";
+import { getMDXComponent } from "mdx-bundler/client";
+import React, { useEffect, useMemo } from "react";
+import { getFileBySlug, getFiles } from "../../lib/mdx";
+import MDXComponents from "../../components/content/MDXComponent";
+import TableOfContents, {
+  HeadingScrollSpy,
+} from "../../components/content/TableOfContents";
+import useScrollSpy from "../../hooks/useScrollspy";
+import styled from "styled-components";
+import CustomLink from "../../components/links/CustomLink";
+import { SiGithub } from "react-icons/si";
+import { HiLink } from "react-icons/hi";
+import { ProjectFrontmatter } from "../../types/frontmatters";
+import { storage } from "../../firebase/config";
+import { ref } from "@firebase/storage";
+import { getDownloadURL } from "firebase/storage";
+import Image from "next/image";
 
 const StyledSection = styled.section`
-    display: grid;
-    grid-template-columns: auto 250px;
-    gap: 2rem;
-    margin-top: 2.4rem;
-    margin-bottom: 3.6rem;
+  display: grid;
+  grid-template-columns: auto 250px;
+  gap: 2rem;
+  margin-top: 2.4rem;
+  margin-bottom: 3.6rem;
 
-    @media (max-width: 1024px) {
-        display: flex;
-    }
-`
+  @media (max-width: 1024px) {
+    display: flex;
+  }
+`;
 
 const Frontmatter = styled.div`
-    .front__title {
-        margin-top: 0.8rem;
-        font-size: 3.6rem;
-    }
+  .front__title {
+    margin-top: 0.8rem;
+    font-size: 3.6rem;
+  }
 
-    .front__description {
-        margin: 0.8rem 0;
-        font-size: 1.4rem;
-    }
+  .front__description {
+    margin: 0.8rem 0;
+    font-size: 1.4rem;
+  }
 
-    .front__itens {
-        display: flex;
-        gap: 2.4rem;
-    }
+  .front__itens {
+    display: flex;
+    gap: 2.4rem;
+  }
 
-    .cover__img {
-        border-radius: .8rem;
-    }
-`
+  .cover__img {
+    border-radius: 0.8rem;
+  }
+`;
 
 const FrontItem = styled.div`
-    display: flex;
-    align-items: center;
-    gap: .8rem;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
 
-    .front__icon {
-        font-size: 2rem;
-    }
-`
+  .front__icon {
+    font-size: 2rem;
+  }
+`;
 
 const ProjectAside = styled.aside`
-    .wrapper__table {
-        position: sticky;
-        top: 10rem;
-    }
+  .wrapper__table {
+    position: sticky;
+    top: 10rem;
+  }
 
-    @media (max-width: 1024px) {
-        display: none;
-    }
-`
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
 
 const ProjectAll = styled.section`
-    margin: 0 4%;
-`
+  margin: 0 4%;
+`;
 
 const Divider = styled.hr`
-    margin-top: 2.4rem;
-`
+  margin-top: 2.4rem;
+`;
 
 type ProjectPageProps = {
-    code: string
-    frontmatter: ProjectFrontmatter
-    cover: string
-}
+  code: string;
+  frontmatter: ProjectFrontmatter;
+  cover: string;
+};
 
 const ProjectPage = (props: ProjectPageProps) => {
-    const Component = useMemo(() => getMDXComponent(props.code), [props.code])
-    const activeSection = useScrollSpy();
-    const [toc, setToc] = React.useState<HeadingScrollSpy>();
+  const Component = useMemo(() => getMDXComponent(props.code), [props.code]);
+  const activeSection = useScrollSpy();
+  const [toc, setToc] = React.useState<HeadingScrollSpy>();
 
-    useEffect(() => {
-        const headings = document.querySelectorAll('.mdx h1, .mdx h2, .mdx h3')
-        const headingArr: HeadingScrollSpy = []
-        headings.forEach((heading) => {
-            const id = heading.id
-            const text = heading.textContent + ''
-            headingArr.push({ id, text })
-        })
-        setToc(headingArr)
-    }, [])
+  useEffect(() => {
+    const headings = document.querySelectorAll(".mdx h1, .mdx h2, .mdx h3");
+    const headingArr: HeadingScrollSpy = [];
+    headings.forEach((heading) => {
+      const id = heading.id;
+      const text = heading.textContent + "";
+      headingArr.push({ id, text });
+    });
+    setToc(headingArr);
+  }, []);
 
-    return (
-        <ProjectAll>
-            <Frontmatter>
-                <Image alt="" src={props.cover} className="cover__img" layout="responsive" width={1300} height={650} />
-                <h1 className='front__title'>{props.frontmatter.title}</h1>
-                <p className='front__description'>
-                    {props.frontmatter.description}
-                </p>
-                <div className='front__itens'>
-                    {props.frontmatter.github && (
-                        <FrontItem>
-                            <SiGithub className='front__icon' />
-                            <CustomLink
-                                onClick={() =>
-                                    console.log('clicou')
-                                }
-                                href={props.frontmatter.github}
-                                >
-                                Repositório
-                            </CustomLink>
-                        </FrontItem>
-                    )}
-                    {props.frontmatter.link && (
-                        <FrontItem>
-                            <HiLink className='front__icon' />
-                            <CustomLink
-                                href={props.frontmatter.link}
-                                onClick={() =>
-                                    console.log('clicou')
-                                }
-                                >
-                                Projeto no ar
-                            </CustomLink>
-                        </FrontItem>
-                    )}
-                </div>
-
-            </Frontmatter>
-            <Divider />
-            <StyledSection>
-                <article className="mdx prose projects">
-                    <Component 
-                        components={
-                            {
-                                ...MDXComponents
-                            } as any
-                        }
-                        />
-                </article>
-                <ProjectAside>
-                    <div className="wrapper__table">
-                        <TableOfContents
-                            toc={toc}
-                            activeSection={activeSection}
-                            />
-                    </div>
-                </ProjectAside>
-            </StyledSection>
-        </ProjectAll>
-    );
-}
+  return (
+    <ProjectAll>
+      <Frontmatter>
+        <Image
+          alt=""
+          src={props.cover}
+          className="cover__img"
+          layout="responsive"
+          width={1300}
+          height={650}
+        />
+        <h1 className="front__title">{props.frontmatter.title}</h1>
+        <p className="front__description">{props.frontmatter.description}</p>
+        <div className="front__itens">
+          {props.frontmatter.github && (
+            <FrontItem>
+              <SiGithub className="front__icon" />
+              <CustomLink
+                onClick={() => console.log("clicou")}
+                href={props.frontmatter.github}
+              >
+                Repositório
+              </CustomLink>
+            </FrontItem>
+          )}
+          {props.frontmatter.link && (
+            <FrontItem>
+              <HiLink className="front__icon" />
+              <CustomLink
+                href={props.frontmatter.link}
+                onClick={() => console.log("clicou")}
+              >
+                Projeto no ar
+              </CustomLink>
+            </FrontItem>
+          )}
+        </div>
+      </Frontmatter>
+      <Divider />
+      <StyledSection>
+        <article className="mdx prose projects">
+          <Component
+            components={
+              {
+                ...MDXComponents,
+              } as any
+            }
+          />
+        </article>
+        <ProjectAside>
+          <div className="wrapper__table">
+            <TableOfContents toc={toc} activeSection={activeSection} />
+          </div>
+        </ProjectAside>
+      </StyledSection>
+    </ProjectAll>
+  );
+};
 
 export default ProjectPage;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const posts = await getFiles('projects');
+  const posts = await getFiles("projects");
 
-    return {
-        paths: posts.map((p) => ({
-            params: {
-                slug: p.replace(/\.mdx/, '')
-            },
-        })),
-        fallback: false,
-    }
-}
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, ""),
+      },
+    })),
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const post = await getFileBySlug('projects', params?.slug as string)
-    const fileName = post.frontmatter.cover
-    const imageRef = ref(storage, fileName)
-    const cover = await getDownloadURL(imageRef)
+  const post = await getFileBySlug("projects", params?.slug as string);
+  const fileName = post.frontmatter.cover;
+  const imageRef = ref(storage, fileName);
+  const cover = await getDownloadURL(imageRef);
 
-    return {
-        props: {cover, ...post},
-    }
-}
+  return {
+    props: { cover, ...post },
+  };
+};
